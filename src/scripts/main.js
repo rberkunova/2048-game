e8e13149ec20d9effcc6c1bf86f771858145e437
rberkunova <161239134+rberkunova@users.noreply.github.com>
2025-02-24 20:03:56 +0200

    clone 2048-game

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const Game = require('../modules/Game.class');
+const game = new Game();
+
+function initializeGame() {
+  const startButton = document.querySelector('.button');
+
+  if (startButton) {
+    startButton.addEventListener('click', () => {
+      // Перевірка статусу гри перед запуском або перезапуском
+      if (game.getStatus() === 'idle') {
+        game.start(); // Запускає гру
+      } else if (game.getStatus() === 'playing') {
+        game.restart(); // Перезапускає гру
+      }
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', initializeGame);
